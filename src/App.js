26591c0a187c55370c5aeb626a10e50c9be94d5d
Jekaterina Sjomkina <olympik2010@inbox.lv>
2021-09-08 19:52:55 +0300

    fix

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { connect } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { Container } from './components/Container/Container';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import { Notification } from './components/Notification/Notification';
+import Filter from './components/Filter/Filter';
+import { H1Styled, H2Styled } from './App.styles';
+
+const App = ({ contacts }) => {
+  return (
+    <Container>
+      <H1Styled>Phonebook</H1Styled>
+      <H2Styled>Add contact</H2Styled>
+      <ContactForm />
+
+
+      {contacts.length > 0 ? (
+        <>
+          <Filter
+            id={uuidv4()}
+            label={'Find contacts by name'}
+            placeholder={'Boris Britva'}
+            name={'search'}
+          />
+          <ContactList />
+        </>
+      ) : (
+        <Notification text={'You don`t have any contacts'} />
+      )}
+    </Container>
+  );
+};
+
+const mapStateToProps = state => ({
+  contacts: state.contactList.contacts,
+});
+
+export default connect(mapStateToProps)(App);
